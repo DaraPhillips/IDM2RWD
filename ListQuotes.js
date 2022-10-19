1440ee5503b81696b652cc4606630ad0c3cff592
DaraPhillips <114760819+DaraPhillips@users.noreply.github.com>
2022-10-19 09:55:42 +0100

    ListQuotesJs
    20 Quotes

---
+++ ListQuotes.js
@@ -0,0 +1,96 @@
+localQuotes
+
+const localQuotes = [
+  {
+    text: “Never wrestle with pigs. You both get dirty and the pig likes it.”,
+    author: "George Bernard Shaw"
+  },
+  {
+    text: "“I think being a woman is like being Irish... Everyone says you're important and nice, but you take second place all the time.”",
+    author: "Iris Murdoch"
+  },
+   {
+    text: "Being Irish, he had an abiding sense of tragedy, which sustained him through temporary periods of joy.",
+    author: "William Butler Yeats"
+  },
+   {
+    text: "Your battles inspired me - not the obvious material battles but those that were fought and won behind your forehead.",
+    author: "James Joyce"
+  },
+   {
+    text: "I'm Irish!...When I feel well I feel better than anyone, when I am in pain I yell at the top of my lungs, and when I am dead I shall be deader than anybody.",
+    author: "Morgan Llywelyn"
+  }
+  {
+    text: "The sea, the snotgreen sea, the scrotumtightening sea.",
+    author: "James Joyce, Ulysses"
+  },
+  {
+    text: "To be Irish is to know that in the end the world will break your heart.",
+    author: "Daniel Patrick Moynihan"
+  },
+  {
+    text: "The great Gaels of Ireland are the men that God made mad,
+    For all their wars are merry, and all their songs are sad.",
+    author: "G.K. Chesterton, The Ballad of the White Horse"
+  },
+  {
+    text: "The first time I read an excellent book,
+    It is to me just as if I had gained a new friend.
+    When I read a book over I have perused before,
+    It resembles the meeting with an old one.
+    ",
+    author: "Oliver Goldsmith"
+  },
+  {
+    text: "We’re nothing if we’re not loved.
+    When you meet somebody who is more important to you than yourself,
+    That has to be the most important thing in life, really.",
+    author: "Maeve Binchy"
+  },
+  {
+    text: "All I ask is the chance to prove
+    That money can’t make me happy.",
+    author: "Spike Milligan"
+  },
+  {
+    text: "We all want progress, but if you’re on the wrong road, progress means doing an about-turn and walking back to the right road; in that case, the man who turns back soonest is the most progressive.",
+    author: "CS Lewis"
+  },
+  {
+    text: "I know that I shall meet my fate
+    Somewhere among the clouds above;
+    Those that I fight I do not hate
+    Those that I guard I do not love.",
+    author: "WB Yeats"
+  },
+  {
+    text: "Nature magically suits a man to his fortunes, by making them the fruit of his character.",
+    author: "Ralph Waldo Emerson"
+  },
+  {
+    text: "There are only two tragedies in life: one is not getting what one wants, and the other is getting it.",
+    author: "Oscar Wilde"
+  },
+  {
+    text: "The more I see of the moneyed classes, the more I understand the guillotine!",
+    author: "George Bernard Shaw"
+  },
+  {
+    text: "The Irish seem to have more fire about them than the Scots.",
+    author: "Sean Connery"
+  },
+  {
+    text: "We are ready to die and shall die cheerfully and proudly, you must not grieve for all of this.
+    ",
+    author: " Padraig Pearse"
+  },
+  {
+    text: "The Irish do not want anyone to wish them well; they want everyone to wish their enemies ill.",
+    author: "Harold Nicolson"
+  },
+  {
+    text: "When Irish eyes are smiling, watch your step.",
+    author: "Gerald Kersh"
+  },
+]
