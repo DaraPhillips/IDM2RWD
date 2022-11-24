fe9dde975b9e199493bbcac8f96a357615d3cf0a
DaraPhillips <114760819+DaraPhillips@users.noreply.github.com>
2022-11-24 22:22:03 +0000

    Surfer Lingo

---
+++ Script.3.js
@@ -0,0 +1,98 @@
+[
+    {
+      "text": "360",
+      "author": "a surf move consisting of turning the surfboard at a 360-degree angle while on the face of a wave"
+    },
+    {
+      "text": "A-frame",
+      "author": "a wave-shaped like a peak that breaks both left and right, equally"
+    },
+     {
+      "text": "Aggro",
+      "author": "aggressive surfing/surfer"
+    },
+     {
+      "text": "Air or Aerial",
+      "author": "is a maneuver in which the board leaves the surface or the water/wave"
+    },
+     {
+      "text": "Akaw",
+      "author": "awesome, cool"
+    },
+    {
+      "text": "Amped",
+      "author": "feeling excited, pumped up"
+    },
+    {
+      "text": "Ankle slappers",
+      "author": "waves that are too small to ride"
+    },
+    {
+      "text": "Backdoor ",
+      "author": "going inside a tube/barrel, also known as the curl of the wave, from behind its peak"
+    },
+    {
+      "text": "Bailing",
+      "author": "Jumping off your board into the water in order to avoid a bad encounter"
+    },
+    {
+      "text": "Barney",
+      "author": "a surfer that is not cool, untalented, rookie"
+    },
+    {
+      "text": "Barrel",
+      "author": "a tube, the curl of the wave, the hollow part of a wave when it is breaking, and one of the most sought after things in surfing"
+    },
+    {
+      "text": "Beach break",
+      "author": "the places where the waves break over sandbars"
+    },
+    {
+      "text": "Benny",
+      "author": "a person who is not a local"
+    },
+    {
+      "text": "Bomb",
+      "author": "a massive wave"
+    },
+    {
+      "text": "Bottom turn",
+      "author": "a turn that is made at the bottom of the wave; a very important maneuver that sets the tone for the ride"
+    },
+    {
+      "text": "Break",
+      "author": "when the swell of the water breaks, turning into waves and white water"
+    },
+    {
+      "text": "Caught inside",
+      "author": "being caught between the shoreline and the breaking waves"
+    },
+    {
+      "text": "Choppy ",
+      "author": " rough waves due to wind conditions"
+    },
+    {
+      "text": "Clean wave",
+      "author": "a smooth wave, with no bumps"
+    },
+    {
+        "text": "Heavy",
+        "author": "big, awesome waves that are sometimes dangerous"
+      },
+      {
+        "text": "Hit the lip",
+        "author": "when a surfer turns up their surfboard to hit the falling lip of the wave, allowing the surfboard to be smashed down"
+      },
+      {
+        "text": "Kook",
+        "author": "a rookie surfer or someone who isn’t very good at surfing"
+      },
+      {
+        "text": "Line up",
+        "author": "the area in the water, away from the swell, where the surfers wait to get their turn at catching a wave"
+      },
+    {
+      "text": "Hang Ten",
+      "author": "riding a surfboard with both feet placed on the nose of the board and your toes hanging over the nose"
+    }
+  ]
